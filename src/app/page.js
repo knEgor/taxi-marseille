2784251373324fec08be253dff0f4a882479b7a7
Egor Pyzhov <kn24-ye.pyzhov@nubip.edu.ua>
2025-07-13 17:44:25 +0300

    Додано гугл карту

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,28 @@
-import Image from "next/image";
 import styles from "./page.module.css";
+import Map from "@/components/ui/map";
+import { Field, Input } from "@chakra-ui/react"
+
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-      <input placeholder="звідки"/>
-      <input placeholder="куди"/>
+        <Field.Root required>
+          <Field.Label>
+            Місце посадки <Field.RequiredIndicator />
+          </Field.Label>
+          <Input placeholder="Сквирський провулок" />
+        </Field.Root>
+
+        <Field.Root required>
+          <Field.Label>
+           Пункт призначення  <Field.RequiredIndicator />
+          </Field.Label>
+          <Input placeholder="Горіхуватський шлях 7а" />
+        </Field.Root>
+        <div style={{ width: '100%', height: '400px', marginTop: '2rem' }}>
+          <Map googleMapsApiKey={process.env.GOOGLE_MAPS_API_KEY} />
+        </div>
       </main>
     </div>
   );
---
+++ src/components/ui/map.jsx
@@ -0,0 +1,52 @@
+'use client'
+
+import React from 'react'
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api'
+
+const containerStyle = {
+    width: '100%',
+    height: '100%',
+}
+
+const center = {
+    lat: 50.4501,
+    lng: 30.5234,
+}
+
+function Map({ googleMapsApiKey }) {
+    const { isLoaded } = useJsApiLoader({
+        id: 'google-map-script',
+        googleMapsApiKey: googleMapsApiKey,
+    })
+
+    const [map, setMap] = React.useState(null)
+
+    const onLoad = React.useCallback(function callback(map) {
+        // This is just an example of getting and using the map instance!!! don't just blindly copy!
+        const bounds = new window.google.maps.LatLngBounds(center)
+        map.fitBounds(bounds)
+
+        setMap(map)
+    }, [])
+
+    const onUnmount = React.useCallback(function callback(map) {
+        setMap(null)
+    }, [])
+
+    return isLoaded ? (
+        <GoogleMap
+            mapContainerStyle={containerStyle}
+            center={center}
+            zoom={10}
+            onLoad={onLoad}
+            onUnmount={onUnmount}
+        >
+            {/* Child components, such as markers, info windows, etc. */}
+            <></>
+        </GoogleMap>
+    ) : (
+        <></>
+    )
+}
+
+export default React.memo(Map)
